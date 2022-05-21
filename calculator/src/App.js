8e4a9de1a9e6fe71959dbfedcca8a1c8aa4719d6
nagaarjunsr <nagaarjunsr@gmail.com>
2022-05-21 15:57:52 +0400

    Steps 1 to 3

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [entries, setEntries] = useState();
+  const [display, setDisplay] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{display}</div>
+      <div>
+        <button onClick={() => { setDisplay(1) }} value={1}>1</button>
+        <button onClick={() => { setDisplay(2) }} value={2}>2</button>
+        <button onClick={() => { setDisplay(3) }} value={3}>3</button>
+      </div>
+      <div>
+        <button onClick={() => { setDisplay(4) }} value={4}>4</button>
+        <button onClick={() => { setDisplay(5) }} value={5}>5</button>
+        <button onClick={() => { setDisplay(6) }} value={6}>6</button>
+      </div>
+      <div>
+        <button onClick={() => { setDisplay(7) }} value={7}>7</button>
+        <button onClick={() => { setDisplay(8) }} value={8}>8</button>
+        <button onClick={() => { setDisplay(9) }} value={9}>9</button>
+      </div>
     </div>
   );
 }
